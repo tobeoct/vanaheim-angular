c9a6e2c21b8917f55307574986de968c7f28a140
tobeoct <tobe.onyema@gmail.com>
2021-05-18 05:29:52 +0100

    c

--- server/app.ts
+++ server/app.ts
@@ -78,8 +78,8 @@ this.webPush.setVapidDetails('mailto:sender@example.com', publicVapidKey, privat
         app.get('*', function(req:any, res:any) {
           console.log( req.session);
           let p = process.env.NODE_ENV=="production"?"": "dist/";
-          let b = process.env.NODE_ENV=="production"?"/": "../";
-          res.sendFile(p+'vanaheim/index.html',{ root: path.resolve(__dirname, b)  })
+          let b = process.env.NODE_ENV=="production"?"": "../";
+          res.sendFile(p+'dist/vanaheim/index.html',{ root: path.resolve(__dirname, b)  })
         })
         
         app.use(inject(sessionResponseAuthorisation))
