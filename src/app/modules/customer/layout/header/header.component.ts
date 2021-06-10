72720122899287dc6c4e8412fbd82d9663867e86
tobeoct <tobe.onyema@gmail.com>
2021-06-10 08:38:50 +0100

    emoji update

--- src/app/modules/customer/layout/header/header.component.ts
+++ src/app/modules/customer/layout/header/header.component.ts
@@ -16,6 +16,8 @@ export class HeaderComponent implements OnInit {
 user:User;
 greetingSubject:BehaviorSubject<string> = new BehaviorSubject<string>('');
 greeting$:Observable<string> = this.greetingSubject.asObservable();
+emojiSubject:BehaviorSubject<string> = new BehaviorSubject<string>('sun');
+emoji$:Observable<string> = this.emojiSubject.asObservable();
   constructor(private _utility:Utility, private _router:Router, private _authService:AuthService) { }
 
   ngOnInit(): void {
@@ -32,16 +34,21 @@ greeting$:Observable<string> = this.greetingSubject.asObservable();
   }
   timer$ = timer(0,50000).subscribe(c=>{
     if(moment().isBetween(moment().startOf("day"), moment().startOf("day").add("hours",4))){
+      this.emojiSubject.next("crescent");
       this.greetingSubject.next("You should be sleeping");
     }else if(moment().isBetween(moment().startOf("day").add("hours",4), moment().startOf("day").add("hours",7))){
+      this.emojiSubject.next("sun-smile");
       this.greetingSubject.next("Rise and Shine");
     }
     else if(moment().isBetween(moment().startOf("day").add("hours",7), moment().startOf("day").add("hours",12))){
+      this.emojiSubject.next("sun");
       this.greetingSubject.next("Good Morning");
     }
     else if(moment().isBetween(moment().startOf("day").add("hours",12), moment().startOf("day").add("hours",18))){
+      this.emojiSubject.next("sun");
       this.greetingSubject.next("Good Afternoon");
     }else{
+      this.emojiSubject.next("moon");
       this.greetingSubject.next("Good Evening");
     }
   })
