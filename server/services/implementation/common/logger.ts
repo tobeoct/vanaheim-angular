e0ce3e74e28c89b36a90c2c2830b579594c51c78
tobeoct <tobe.onyema@gmail.com>
2021-10-27 19:54:13 +0100

    comment out logger.ts

--- server/services/implementation/common/logger.ts
+++ server/services/implementation/common/logger.ts
@@ -1,20 +1,20 @@
-// import bunyan, { RotatingFileStream } from "bunyan";
-import * as bunyan from "bunyan";
-import { RotatingFileStream } from "bunyan";
-import { existsSync, mkdirSync } from "fs";
-const root =`\\logs`;
-const logPath = `${root}\\vanaheim-log`;
-if(!existsSync(root)){
-   mkdirSync(root,{recursive:true})
-}
+// // import bunyan, { RotatingFileStream } from "bunyan";
+// import * as bunyan from "bunyan";
+// import { RotatingFileStream } from "bunyan";
+// import { existsSync, mkdirSync } from "fs";
+// const root =`\\logs`;
+// const logPath = `${root}\\vanaheim-log`;
+// if(!existsSync(root)){
+//    mkdirSync(root,{recursive:true})
+// }
 
-export const Logger = bunyan.createLogger({
-   name: "vanaheim", 
-   streams: [{
-      stream: new RotatingFileStream({
-         path:logPath,
-         period:"1d",
-         count:10
-      })
-  }]
-});
+// export const Logger = bunyan.createLogger({
+//    name: "vanaheim", 
+//    streams: [{
+//       stream: new RotatingFileStream({
+//          path:logPath,
+//          period:"1d",
+//          count:10
+//       })
+//   }]
+// });
